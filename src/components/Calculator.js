700b4070fc3f9d5032f478ade5f6cd39bfe3ad54
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-08-31 13:56:14 +0500

    Handle events and Add Calculate.js and Operate.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import { useState } from 'react';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [state, setState] = useState({ total: 0, next: null, operation: null });
+
+  const handleClick = (event) => {
+    const buttonName = event.target.innerText;
+    const result = calculate(state, buttonName);
+    setState(result);
+  };
+
+  const { total, next, operation } = state;
+
   return (
     <div className="container">
       <div className="wrapper">
-        <div className="display">0</div>
-        <div className="btn grey">AC</div>
-        <div className="btn grey">+/-</div>
-        <div className="btn grey">%</div>
-        <div className="btn orange">/</div>
-        <div className="btn grey">7</div>
-        <div className="btn grey">8</div>
-        <div className="btn grey">9</div>
-        <div className="btn orange">x</div>
-        <div className="btn grey">4</div>
-        <div className="btn grey">5</div>
-        <div className="btn grey">6</div>
-        <div className="btn orange">-</div>
-        <div className="btn grey">1</div>
-        <div className="btn grey">2</div>
-        <div className="btn grey">3</div>
-        <div className="btn orange">+</div>
-        <div className="btn grey span">0</div>
-        <div className="btn grey">.</div>
-        <div className="btn orange">=</div>
+        <button type="button" className="display">
+          {total}
+          {operation}
+          {next}
+        </button>
+        <button type="button" className="btn grey" onClick={handleClick}>AC</button>
+        <button type="button" className="btn grey" onClick={handleClick}>+/-</button>
+        <button type="button" className="btn grey" onClick={handleClick}>%</button>
+        <button type="button" className="btn orange" onClick={handleClick}>/</button>
+        <button type="button" className="btn grey" onClick={handleClick}>7</button>
+        <button type="button" className="btn grey" onClick={handleClick}>8</button>
+        <button type="button" className="btn grey" onClick={handleClick}>9</button>
+        <button type="button" className="btn orange" onClick={handleClick}>x</button>
+        <button type="button" className="btn grey" onClick={handleClick}>4</button>
+        <button type="button" className="btn grey" onClick={handleClick}>5</button>
+        <button type="button" className="btn grey" onClick={handleClick}>6</button>
+        <button type="button" className="btn orange" onClick={handleClick}>-</button>
+        <button type="button" className="btn grey" onClick={handleClick}>1</button>
+        <button type="button" className="btn grey" onClick={handleClick}>2</button>
+        <button type="button" className="btn grey" onClick={handleClick}>3</button>
+        <button type="button" className="btn orange" onClick={handleClick}>+</button>
+        <button type="button" className="btn grey span" onClick={handleClick}>0</button>
+        <button type="button" className="btn grey" onClick={handleClick}>.</button>
+        <button type="button" className="btn orange" onClick={handleClick}>=</button>
       </div>
     </div>
   );
