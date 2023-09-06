693077b2844b4ea0f2c82064052f217a655f945c
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-09-06 14:31:34 +0500

    Add Routes and Final Touches

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <section>
+        <div className="container">
+          <h1>Welcome to our Page!</h1>
+          <p>
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            dignissimos iure soluta facere, odio quae, perspiciatis quisquam
+            sint atque voluptatem blanditiis sunt iste laudantium rerum officia
+            earum praesentium laboriosam. Odit accusamus consequatur quam?
+            Deleniti quos tempore itaque cum consequuntur minus ratione omnis,
+            quas qui nam facilis vero cumque officiis repellat?
+            <br />
+          </p>
+          <p>
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            quibusdam officia porro commodi repudiandae quae laborum libero
+            ullam obcaecati voluptatem suscipit necessitatibus odit nemo debitis
+            rerum nisi, nobis asperiores ut a quia animi doloribus!
+            <br />
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
